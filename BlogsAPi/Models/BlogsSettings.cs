fe232041b36adba289e49c656cb6c51f730beb3c
Andri Manuel Zabala Urbaez <andriurbaez05@gmail.com>
2025-05-31 11:10:40 -0400

    Configuracion de la cadena de conexion casi lista, solo falta el servicio que administre la cadena de conexion.

---
+++ BlogsAPi/Models/BlogsSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace BlogsAPi.Models
+{
+    public class BlogsSettings
+    {
+        public string BlogsApi { get; set; } = string.Empty;
+        public string DatabaseName { get; set; } = string.Empty;
+        public string CollectionName { get; set; } = string.Empty;
+    }
+}
