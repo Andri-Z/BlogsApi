c99ccc344951ff1dcfd8031b838cd29350a7102a
Andri Manuel Zabala Urbaez <andriurbaez05@gmail.com>
2025-06-01 17:59:03 -0400

    Eliminacion de controlador y modelo de clase base que trae por defecto la api, cambio de nombre de la propiedad del modelo updateAt, solucion de errores que no permitia arrancar la api

---
+++ BlogsAPi/BlogsService.cs
@@ -0,0 +1,28 @@
+﻿using BlogsAPi.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace BlogsAPi
+{
+    public class BlogsService
+    {
+        private readonly IMongoCollection<Blogs> _blogsCollection;
+        public BlogsService(IOptions<BlogsSettings> options) 
+        {
+            var mongoClient = new MongoClient(options.Value.BlogsApi);
+            var database = mongoClient.GetDatabase(options.Value.DatabaseName);
+            _blogsCollection = database.GetCollection<Blogs>(options.Value.CollectionName);
+        }
+        public async Task<List<Blogs>> GetBlogsAsyn() =>
+            await _blogsCollection.Find(x => true).ToListAsync();
+        public async Task<Blogs> GetBlogsById(ObjectId id) =>
+            await _blogsCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+        public async Task CreateBlogsAsync(Blogs blog) =>
+            await _blogsCollection.InsertOneAsync(blog);
+        public async Task<ReplaceOneResult> UpdateBlogsAsync(ObjectId id, Blogs blog)=>
+            await _blogsCollection.ReplaceOneAsync(x => x.Id == id, blog);
+        public async Task<DeleteResult> DeleteAsync(ObjectId id) =>
+            await _blogsCollection.DeleteOneAsync(x => x.Id == id);
+    }
+}
